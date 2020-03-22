c809a3417288b9cd047a7943536f9f86885a9be0
Veiko Aunapuu <veiko@Veikos-MacBook-Pro.local>
2020-03-22 15:49:42 +0200

    Replace js logic with css class

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,14 +1,4 @@
 //Make TO DO appear done by clicking on it
 $("li").click(function(){
-    //if li is gray
-    if ($(this).css("color") === "rgb(128, 128, 128)") {
-        // turn it black
-        $(this).css("color", "black");
-        $(this).css("text-decoration", "none");
-    } 
-    else {
-        // turn it gray
-        $(this).css("color", "gray");
-        $(this).css("text-decoration", "line-through");
-    }
+    $(this).toggleClass("completed");
 });
